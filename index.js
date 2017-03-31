ef6f9570059f668071bf4a976637e7045d8be689
Lucas <lucasrndl@gmail.com>
2017-03-31 16:28:04 +0200

    soundcloud downloader ok

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import chalk from 'chalk'
+import sc_downloader from './ez-downloader-soundcloud'
 
-console.log(chalk.red("Working :)"))
+sc_downloader.download("https://soundcloud.com/migosatl/quavo-ft-lil-durk-x-cah-out-kinny")
