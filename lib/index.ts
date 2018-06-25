862aac70b7ba87505e05c8f20dedf44c3dc81d69
Lucas Riondel <lucasrndl@gmail.com>
2018-06-25 23:21:42 +0200

    multi download through CLI with no args

--- lib/index.ts
+++ lib/index.ts
@@ -4,12 +4,14 @@ import * as urlParse from 'url-parse';
 import * as ProgressBar from 'progress';
 import * as inquirer from 'inquirer';
 import * as program from 'commander';
+import * as readline from 'readline';
 
 import EzDownloaderSoundcloud from './EzDownloaderSoundcloud';
 import EzDownloaderYoutube from './EzDownloaderYoutube';
 import Tagger, { UserFriendlyTags } from './Tagger';
 
 const debug = debugModule('entry');
+const version = process.env.npm_package_version as string;
 
 const ezDownloaderSoundcloud = new EzDownloaderSoundcloud();
 const ezDownloaderYoutube = new EzDownloaderYoutube();
@@ -64,7 +66,7 @@ const askTagEdition = async (filename: string, coverUrl?: string) => {
 const downloadFromSoundcloud = async (url: string, withTags: boolean) => {
   try {
     const result = await ezDownloaderSoundcloud.download(url, displayProgress);
-    debug(chalk.green('Song downloaded !'));
+    console.log(chalk.green('Song downloaded !'));
     if (withTags) askTagEdition(result.filename, result.coverUrl);
   } catch (e) {
     console.error(chalk.red.bold(e));
@@ -93,34 +95,42 @@ const displayProgress = (chunk: number, total: number) => {
   bar.tick(chunk);
 };
 
+const processUrl = async (url: string) => {
+  debug(`TRACK_URL ${url}`);
+  const parsedUrl = urlParse(url, true);
+  if (parsedUrl.host === 'soundcloud.com' && parsedUrl.hostname === 'soundcloud.com') {
+    await downloadFromSoundcloud(url, !program.notags);
+  }
+  // TODO handle short youtube urls
+  else if (parsedUrl.host === 'www.youtube.com' && parsedUrl.hostname === 'www.youtube.com') {
+    await downloadFromYoutube(url, !program.notags);
+  } else {
+    console.error(chalk.red.bold('Wrong url, service not found.'));
+  }
+};
+
 async function main() {
   program
     .name('ez')
     .arguments('<url ...>')
-    .version(process.env.npm_package_version as string)
+    .version(version)
     .option('-n, --notags', 'Do not ask for tags')
     .parse(process.argv);
 
   const { notags, args } = program;
-  debug(notags ? 'pas de tags' : 'tag');
-  debug(args);
+  debug('URL', args);
+  debug('TAG EDITION', !notags);
 
   if (args.length > 0) {
-    for (const link of args) {
-      debug(`TRACK_URL ${link}`);
-      const parsedUrl = urlParse(link, true);
-      if (parsedUrl.host === 'soundcloud.com' && parsedUrl.hostname === 'soundcloud.com') {
-        await downloadFromSoundcloud(link, !program.notags);
-      }
-      // TODO handle short youtube links
-      else if (parsedUrl.host === 'www.youtube.com' && parsedUrl.hostname === 'www.youtube.com') {
-        await downloadFromYoutube(link, !program.notags);
-      } else {
-        debug(chalk.red('Wrong url, service not found.'));
-      }
-    }
+    for (const url of args) await processUrl(url);
   } else {
-    // TODO stdin readder
+    console.log(chalk.green(`ez cli v${version}\nplease type url`));
+    const rlInterface = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: true,
+    });
+    rlInterface.on('line', async (line: string) => await processUrl(line.trim()));
   }
 }
 
